510d7a740dceb2d4cd5d0bcd2331e6b469ad1049
Eng.Adel Alsaedi <ans.adil@gmail.com>
2019-08-17 08:11:37 +0300

    update env

--- lib/env.dart
+++ lib/env.dart
@@ -1,6 +1,6 @@
 class ProdEnv {
 final String baseurl="https://esafety.ans-sa.com/";
-// final String baseurl="http://192.168.1.7/";
+// final String baseurl="http://192.168.1.5/";
 final String api="api/";
 
 String get baseurl_api {
